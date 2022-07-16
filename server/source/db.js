93f7ba5f1299f8237fb1e23d20798f9f4db22f61
Emre Güdür <emregudur@gmail.com>
2022-07-16 15:32:31 +0300

    mongo uri changes

--- server/source/db.js
+++ server/source/db.js
@@ -3,8 +3,9 @@ import { uri } from './common'
 
 export default async function () {
   return new Promise((resolve, reject) => {
-    mongoose.set('useUnifiedTopology', true)
-    mongoose.set('useNewUrlParser', true)
+    mongoose.set('bufferTimeoutMS', 600000)
+    // mongoose.set('useUnifiedTopology', true)
+    // mongoose.set('useNewUrlParser', true)
 
     mongoose
       .connect(uri)
